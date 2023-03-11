94c1741da16bff9aa24fd523d98f24c5313fa5a1
hjc2000 <hjc.666@foxmail.com>
2023-03-12 00:02:12 +0800

    在玩一种很新的东西

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,30 +1,145 @@
-﻿string path = @"C:\Users\huang\source\repos\ESP32配网\ConsoleApp1\output.txt";
-FileStream file = File.Open(path, FileMode.Open);
-StreamReader reader = new StreamReader(file);
-FileStream file_to_write = File.Open(@"D:\my_files\workspace\temp\output.txt", FileMode.Create);
-StreamWriter writer = new StreamWriter(file_to_write);
+﻿using ModulatorLib;
 
-string read_data = reader.ReadToEnd();
-string[] nums = read_data.Split(',',
-	StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-int i = 0;
-foreach (string str in nums)
+HtmlForm _form = new HtmlForm()
 {
-	writer.Write(str + ",");
-	i++;
-	if ((i % 20) == 0)
+	new SelectTag("country")
 	{
-		writer.WriteLine();
-	}
+		"China",
+		"Hongkong",
+	},
+	new SelectTag("channel")
+	{
+		"21 (474.0 MHz)",
+		"22 (482.0 MHz)",
+		"23 (490.0 MHz)",
+		"24 (498.0 MHz)",
+		"25 (506.0 MHz)",
+		"26 (514.0 MHz)",
+		"27 (522.0 MHz)",
+		"28 (530.0 MHz)",
+		"29 (538.0 MHz)",
+		"30 (546.0 MHz)",
+		"31 (554.0 MHz)",
+		"32 (562.0 MHz)",
+		"33 (570.0 MHz)",
+		"34 (578.0 MHz)",
+		"35 (586.0 MHz)",
+		"36 (594.0 MHz)",
+		"37 (602.0 MHz)",
+		"38 (610.0 MHz)",
+		"39 (618.0 MHz)",
+		"40 (626.0 MHz)",
+		"41 (634.0 MHz)",
+		"42 (642.0 MHz)",
+		"43 (650.0 MHz)",
+		"44 (658.0 MHz)",
+		"45 (666.0 MHz)",
+		"46 (674.0 MHz)",
+		"47 (682.0 MHz)",
+		"48 (690.0 MHz)",
+		"49 (698.0 MHz)",
+		"50 (706.0 MHz)",
+		"51 (714.0 MHz)",
+		"52 (722.0 MHz)",
+		"53 (730.0 MHz)",
+		"54 (738.0 MHz)",
+		"55 (746.0 MHz)",
+		"56 (754.0 MHz)",
+		"57 (762.0 MHz)",
+		"58 (770.0 MHz)",
+		"59 (778.0 MHz)",
+		"60 (786.0 MHz)",
+		"61 (794.0 MHz)",
+		"62 (802.0 MHz)",
+		"63 (810.0 MHz)",
+		"64 (818.0 MHz)",
+		"65 (826.0 MHz)",
+		"66 (834.0 MHz)",
+		"67 (842.0 MHz)",
+		"68 (850.0 MHz)",
+		"69 (858.0 MHz)",
+	},
+	new SelectTag("freq")
+	{
+		"522.000",
+	},
+	new SelectTag("level")
+	{
+		"45 dBmV",
+		"44 dBmV",
+		"43 dBmV",
+		"42 dBmV",
+		"41 dBmV",
+		"40 dBmV",
+		"39 dBmV",
+		"38 dBmV",
+		"37 dBmV",
+		"36 dBmV",
+		"35 dBmV",
+		"34 dBmV",
+		"33 dBmV",
+		"32 dBmV",
+		"31 dBmV",
+		"30 dBmV",
+		"29 dBmV",
+		"28 dBmV",
+		"27 dBmV",
+		"26 dBmV",
+		"25 dBmV",
+	},
+	new SelectTag("video")
+	{
+		"HDMI",
+	},
+	new SelectTag("audio")
+	{
+		"HDMI AUDIO",
+	},
+	new SelectTag("Caption")
+	{
+		"Off",
+	},
+	new SelectTag("major")
+	{
+		"77",
+	},
+	new SelectTag("minor")
+	{
+		"1"
+	},
+	new SelectTag("LCN1")
+	{
+		"100",
+	},
+	new SelectTag("PN1")
+	{
+		"PVI HD1",
+	},
+	new SelectTag("p2i")
+	{
+		"Auto",
+		"Interlace",
+	},
+	new SelectTag("edid")
+	{
+		"Auto",
+		"1080p",
+		"1080i",
+		"720p",
+	},
+	new SelectTag("send")
+	{
+		"Save",
+		"Cancel",
+	},
+};
+Console.WriteLine(_form.ToString());
+FormItem? item = _form.FindFormItemWithName("country");
+if (item != null)
+{
+	Console.WriteLine("找到的值：" + item.Value);
 }
-//string[] lines = read_data.Split('\n',
-//	StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
-
-//foreach (string line in lines)
-//{
-//	int start_index = line.IndexOf(',') + 1;
-//	int end_index = line.Length - 1 - 1;
-//	string tempStr = line.Substring(start_index, end_index - start_index);
-//	writer.WriteLine(tempStr);
-//}
-writer.Flush();
+else
+{
+	Console.WriteLine("没找到");
+}
---
+++ ModulatorLib/HtmlForm.cs
@@ -0,0 +1,172 @@
+﻿using System.Collections;
+
+namespace ModulatorLib
+{
+	/// <summary>
+	/// 模拟HTML的表单
+	/// </summary>
+	public class HtmlForm : IEnumerable<FormItem>
+	{
+		public string Action { get; set; } = @"http://192.168.1.15/ChannelSetup.htm";
+
+		/// <summary>
+		/// 通过标签的name属性来找到指定的项。表单中的标签的name属性不能重复，所以可以用来
+		/// 唯一标识一个标签
+		/// </summary>
+		/// <param name="name">表单项的name属性的值</param>
+		/// <returns>表单项的引用</returns>
+		public FormItem? FindFormItemWithName(string name)
+		{
+			return Items.Find((item) =>
+			{
+				if (item.Name == name)
+				{
+					return true;
+				}
+
+				return false;
+			});
+		}
+
+		/// <summary>
+		/// 存放表单项的列表
+		/// </summary>
+		public List<FormItem> Items = new List<FormItem>();
+
+		public IEnumerator<FormItem> GetEnumerator()
+		{
+			return Items.GetEnumerator();
+		}
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return Items.GetEnumerator();
+		}
+
+		public void Add(FormItem item)
+		{
+			Items.Add(item);
+		}
+
+		/// <summary>
+		/// 获得表单的http post请求体字符串
+		/// </summary>
+		/// <returns></returns>
+		public override string ToString()
+		{
+			string re_str = string.Empty;
+			foreach (FormItem item in Items)
+			{
+				re_str += item.ToString() + "&";
+			}
+
+			re_str = re_str.Substring(0, re_str.Length - 1);
+			return re_str;
+		}
+	}
+
+	/// <summary>
+	/// 表单项基类
+	/// </summary>
+	public class FormItem
+	{
+		public FormItem() { }
+		public FormItem(string name)
+		{
+			Name = name;
+		}
+		public FormItem(string name, string value)
+		{
+			Name = name;
+			Value = value;
+		}
+		/// <summary>
+		/// 表单项的name属性
+		/// </summary>
+		public virtual string Name
+		{
+			get => Attributes["name"];
+			set => Attributes["name"] = value;
+		}
+
+		/// <summary>
+		/// 表单项的value属性
+		/// </summary>
+		public virtual string Value
+		{
+			get => Attributes["value"];
+			set => Attributes["value"] = value;
+		}
+		/// <summary>
+		/// 标签属性
+		/// </summary>
+		public Dictionary<string, string> Attributes { get; set; } = new Dictionary<string, string>()
+		{
+			{"name",string.Empty},
+			{"value",string.Empty},
+		};
+
+		/// <summary>
+		/// 返回名值对的字符串。格式为：key=value，例如：name=张三
+		/// </summary>
+		/// <returns></returns>
+		public override string ToString()
+		{
+			return $"{Name}={Value}";
+		}
+	}
+
+	/// <summary>
+	/// 模拟select标签
+	/// </summary>
+	public class SelectTag : FormItem, IEnumerable<string>
+	{
+		public SelectTag() { }
+		public SelectTag(string name) : base(name) { }
+		public SelectTag(string name, string value) : base(name, value) { }
+
+		private string? _value = null;
+		public override string Value
+		{
+			get
+			{
+				if (_value == null)
+				{
+					try
+					{
+						_value = Options[0];
+						/*使用override关键字重写后，通过基类的引用访问Value属性时，访问到的
+						 是派生类的Value属性。但是，派生类中可以通过base.Value来访问基类的
+						Value属性，此行为不影响属性的多态。*/
+						base.Value = _value;
+					}
+					catch
+					{
+						_value = string.Empty;
+					}
+				}
+
+				return _value;
+			}
+			set => _value = value;
+		}
+
+		/// <summary>
+		/// select标签的可选项
+		/// </summary>
+		public List<string> Options { get; set; } = new List<string>();
+
+		public IEnumerator<string> GetEnumerator()
+		{
+			return Options.GetEnumerator();
+		}
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return Options.GetEnumerator();
+		}
+
+		public void Add(string name)
+		{
+			Options.Add(name);
+		}
+	}
+}

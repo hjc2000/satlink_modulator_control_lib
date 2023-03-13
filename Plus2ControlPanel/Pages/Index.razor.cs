86ab3f881396020f64f7aa4f73ae60dcdde34177
hjc2000 <hjc.666@foxmail.com>
2023-03-13 20:52:06 +0800

    将Index组件的C#代码放到专门的cs文件中。执行代码清理

---
+++ Plus2ControlPanel/Pages/Index.razor.cs
@@ -0,0 +1,70 @@
+﻿using ModulatorLib;
+
+namespace Plus2ControlPanel.Pages
+{
+	public partial class Index
+	{
+#nullable enable
+		private HtmlForm _form = new();
+		private HttpClient _client = new();
+
+		//string _requestUrl = @"http://localhost:8051/Modulator_files/MainInfo.html";
+		private string _requestUrl = @"http://192.168.1.15/ChannelSetup.htm";
+		private SelectTag? _channelSelectTag = null;
+		private int _channelIndex = 0;
+
+		private void ParseHtml(string response)
+		{
+			_form.ParseHtml(response);
+			_form.Items.Remove("send");
+			_form.Items.Remove("freq");
+
+			_channelSelectTag = (SelectTag)_form["channel"];
+			_channelIndex = _channelSelectTag.Options.FindIndex((str) =>
+			{
+				if (str == _channelSelectTag.Value)
+				{
+					return true;
+				}
+
+				return false;
+			});
+		}
+
+		private async void OnSubmitButtonClick()
+		{
+			string postStr = _form.ToString();
+			StringContent stringContent = new StringContent(postStr);
+			HttpResponseMessage message = await _client.PostAsync(_requestUrl, stringContent);
+			string response = await message.Content.ReadAsStringAsync();
+			ParseHtml(response);
+			StateHasChanged();
+		}
+
+		private void OnNextFreq()
+		{
+			if (_channelSelectTag != null)
+			{
+				_channelIndex++;
+				if (_channelIndex >= _channelSelectTag.Options.Count)
+				{
+					_channelIndex = 0;
+				}
+
+				_channelSelectTag.Value = _channelSelectTag.Options[_channelIndex];
+				OnSubmitButtonClick();
+			}
+		}
+
+		private async Task OnFlush()
+		{
+			HttpResponseMessage responseMessage = await _client.GetAsync(_requestUrl);
+			if (responseMessage.IsSuccessStatusCode)
+			{
+				HttpContent content = responseMessage.Content;
+				string response = await content.ReadAsStringAsync();
+				ParseHtml(response);
+			}
+		}
+	}
+}
